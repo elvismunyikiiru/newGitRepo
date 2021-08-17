e48d39b4f4ae927fedb5bf8a60b01e7b51eb752a
su8code <88676535+su8code@users.noreply.github.com>
2021-08-17 17:41:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-window.location.replace("http://stackoverflow.com");
+//window.location.replace("http://stackoverflow.com");
+
+var url = "https://su8code.github.io/newGitRepo/myJson.json";
+   var data;
+   var numCtx = 0;
+  
+var jsonFile = new XMLHttpRequest();
+    jsonFile.open("GET",url,true);    
+    jsonFile.send();
+
+    jsonFile.onreadystatechange = function() {
+        if (jsonFile.readyState== 4 && jsonFile.status == 200) {
+            //document.getElementById("id-of-element").innerHTML = jsonFile.responseText;
+             data = JSON.parse(jsonFile.responseText);
+             numCtx = data.number;
+            //console.log(data.number)
+
+/*
+            const links = document.getElementsByTagName('a');
+
+            for(var i; i<links.length; l++){
+               
+            } */
+        }
+        else{
+           // console.log("Json parse Error");
+        }
+
+        for(var a=0;a<numCtx; a++){
+            console.log(data.savedChannels[a]);
+        }
+
+     }
